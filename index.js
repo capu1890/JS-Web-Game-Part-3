847551d53c201a864dccf1beba556ab52de27ff5
Craig Caparaz <cappuccino1890@Craigs-Air.lan1>
2022-01-15 16:53:56 -0800

    js activity

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
+
+function move(image) {
+    image.style.position = 'fixed';
+    
+    function moveToCoordinate (left, bottom) {
+        image.style.left = left + 'px';
+        image.style.bottom = bottom + 'px';
+    }
+    return {
+        to: moveToCoordinate
+    }
+}
+
+move(newImage('assets/green-character.gif')).to(100, 250);
 
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
@@ -14,7 +25,6 @@ newImage('assets/pillar.png', 350, 250)
 newImage('assets/pine-tree.png', 450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
-
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
